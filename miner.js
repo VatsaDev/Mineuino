4fcc58c1d446e159b6fd377ceea9db49e8c8b07d
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2021-06-06 10:06:31 +0530

    Update miner.js

--- miner.js
+++ miner.js
@@ -7,6 +7,7 @@ let diff = "LOW";
 let SHA1 = new Hashes.SHA1();
 let timeDiff;
 let clickcount = 0;
+let socket;
 
 let url_string = window.location;
 let url = new URL(url_string);
@@ -16,67 +17,10 @@ let username = document
 let rigid = "mineuino-miner";
 
 if (location.protocol !== "https:") {
-  let socket = new WebSocket("ws://51.15.127.80:14808", null, 5000, 5);
-  socket.onmessage = function (msg) {
-    serverMessage = msg.data.replace("\n", "");
-    if (serverMessage.includes("2.")) {
-      console.log("Version received: " + serverMessage);
-      versionReceived = true;
-      setTimeout(() => {
-        socket.send("JOB," + username + "," + diff);
-      }, 500);
-    }
-
-    if (
-      (versionReceived && serverMessage === "GOOD") ||
-      serverMessage === "BLOCK"
-    ) {
-      console.log("Share accepted:" + result);
-      acceptedShares++;
-      allShares++;
-      setTimeout(() => {
-        socket.send("JOB," + username + "," + diff);
-      }, 500);
-    }
-
-    if (versionReceived && serverMessage === "BAD") {
-      console.log("Share rejected");
-      allShares++;
-      setTimeout(() => {
-        socket.send("JOB," + username + "," + diff);
-      }, 500);
-    }
-
-    if (versionReceived && serverMessage.length > 40) {
-      console.log("Job received: " + serverMessage);
-      job = serverMessage.split(",");
-      difficulty = job[2];
-
-      startTime = new Date();
-      for (result = 0; result < 100 * difficulty + 1; result++) {
-        ducos1 = SHA1.hex(job[0] + result);
-        if (job[1] === ducos1) {
-          endTime = new Date();
-          timeDiff = (endTime - startTime) / 1000;
-          hashrate = (result / timeDiff).toFixed(2);
-          console.log(
-            "Share found: " +
-              result +
-              " Time: " +
-              timeDiff +
-              " Hashrate: " +
-              hashrate
-          );
-          socket.send(
-            result + "," + hashrate + ",Official Webminer v2.3.6," + rigid
-          ); // send the result to the server
-          break;
-        }
-      }
-    }
-  };
+  socket = new WebSocket("ws://51.15.127.80:14808", null, 5000, 5);
 } else {
-  let socket = new WebSocket("wss://51.15.127.80:15808", null, 5000, 5);
+  socket = new WebSocket("wss://51.15.127.80:15808", null, 5000, 5);
+}
   socket.onmessage = function (msg) {
     serverMessage = msg.data.replace("\n", "");
     if (serverMessage.includes("2.")) {
@@ -135,4 +79,3 @@ if (location.protocol !== "https:") {
       }
     }
   };
-}
